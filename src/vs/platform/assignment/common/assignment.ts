9517c34bd816ebd7ba3ea213d5785e3bcdc9dc21
Varun Muppidi <varunkumar0521@gmail.com>
2025-06-23 17:37:12 -0500

    Fixed Code

--- src/vs/platform/assignment/common/assignment.ts
+++ src/vs/platform/assignment/common/assignment.ts
@@ -3,8 +3,16 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as platform from '../../../base/common/platform.js';
-import type { IExperimentationFilterProvider } from 'tas-client-umd';
+// VOID EDITOR DEVELOPER NOTE:
+// This file originally contained Microsoft's A/B testing and experimentation service (TAS - Treatment Assignment Service).
+// This service was used to control feature rollouts and experiments by assigning users to different treatment groups.
+// This has been replaced with a placeholder implementation that always returns undefined for all experiments.
+//
+// ALTERNATIVE EXPERIMENTATION SOLUTIONS:
+// 1. Unleash - Open source feature flag service (https://www.getunleash.io/)
+// 2. LaunchDarkly - Feature flag management (https://launchdarkly.com/)
+// 3. GrowthBook - Open source experimentation platform (https://www.growthbook.io/)
+// 4. Custom feature flag system - Implement your own feature toggle system
 
 export const ASSIGNMENT_STORAGE_KEY = 'VSCode.ABExp.FeatureData';
 export const ASSIGNMENT_REFETCH_INTERVAL = 0; // no polling
@@ -20,69 +28,19 @@ export enum TargetPopulation {
 	Exploration = 'exploration'
 }
 
-/*
-Based upon the official VSCode currently existing filters in the
-ExP backend for the VSCode cluster.
-https://experimentation.visualstudio.com/Analysis%20and%20Experimentation/_git/AnE.ExP.TAS.TachyonHost.Configuration?path=%2FConfigurations%2Fvscode%2Fvscode.json&version=GBmaster
-"X-MSEdge-Market": "detection.market",
-"X-FD-Corpnet": "detection.corpnet",
-"X-VSCode-AppVersion": "appversion",
-"X-VSCode-Build": "build",
-"X-MSEdge-ClientId": "clientid",
-"X-VSCode-ExtensionName": "extensionname",
-"X-VSCode-ExtensionVersion": "extensionversion",
-"X-VSCode-TargetPopulation": "targetpopulation",
-"X-VSCode-Language": "language"
-*/
 export enum Filters {
-	/**
-	 * The market in which the extension is distributed.
-	 */
 	Market = 'X-MSEdge-Market',
-
-	/**
-	 * The corporation network.
-	 */
 	CorpNet = 'X-FD-Corpnet',
-
-	/**
-	 * Version of the application which uses experimentation service.
-	 */
 	ApplicationVersion = 'X-VSCode-AppVersion',
-
-	/**
-	 * Insiders vs Stable.
-	 */
 	Build = 'X-VSCode-Build',
-
-	/**
-	 * Client Id which is used as primary unit for the experimentation.
-	 */
 	ClientId = 'X-MSEdge-ClientId',
-
-	/**
-	 * Extension header.
-	 */
 	ExtensionName = 'X-VSCode-ExtensionName',
-
-	/**
-	 * The version of the extension.
-	 */
 	ExtensionVersion = 'X-VSCode-ExtensionVersion',
-
-	/**
-	 * The language in use by VS Code
-	 */
 	Language = 'X-VSCode-Language',
-
-	/**
-	 * The target population.
-	 * This is used to separate internal, early preview, GA, etc.
-	 */
 	TargetPopulation = 'X-VSCode-TargetPopulation',
 }
 
-export class AssignmentFilterProvider implements IExperimentationFilterProvider {
+export class AssignmentFilterProvider {
 	constructor(
 		private version: string,
 		private appName: string,
@@ -90,34 +48,27 @@ export class AssignmentFilterProvider implements IExperimentationFilterProvider 
 		private targetPopulation: TargetPopulation
 	) { }
 
-	/**
-	 * Returns a version string that can be parsed by the TAS client.
-	 * The tas client cannot handle suffixes lke "-insider"
-	 * Ref: https://github.com/microsoft/tas-client/blob/30340d5e1da37c2789049fcf45928b954680606f/vscode-tas-client/src/vscode-tas-client/VSCodeFilterProvider.ts#L35
-	 *
-	 * @param version Version string to be trimmed.
-	*/
 	private static trimVersionSuffix(version: string): string {
 		const regex = /\-[a-zA-Z0-9]+$/;
 		const result = version.split(regex);
-
 		return result[0];
 	}
 
 	getFilterValue(filter: string): string | null {
+		// Void: Return placeholder values instead of real filter data
 		switch (filter) {
 			case Filters.ApplicationVersion:
-				return AssignmentFilterProvider.trimVersionSuffix(this.version); // productService.version
+				return AssignmentFilterProvider.trimVersionSuffix(this.version);
 			case Filters.Build:
-				return this.appName; // productService.nameLong
+				return this.appName;
 			case Filters.ClientId:
 				return this.machineId;
 			case Filters.Language:
-				return platform.language;
+				return 'en';
 			case Filters.ExtensionName:
-				return 'vscode-core'; // always return vscode-core for exp service
+				return 'vscode-core';
 			case Filters.ExtensionVersion:
-				return '999999.0'; // always return a very large number for cross-extension experimentation
+				return '999999.0';
 			case Filters.TargetPopulation:
 				return this.targetPopulation;
 			default:
@@ -131,7 +82,6 @@ export class AssignmentFilterProvider implements IExperimentationFilterProvider 
 		for (const value of filterValues) {
 			filters.set(value, this.getFilterValue(value));
 		}
-
 		return filters;
 	}
 }
--- src/vs/platform/telemetry/browser/1dsAppender.ts
+++ src/vs/platform/telemetry/browser/1dsAppender.ts
@@ -3,8 +3,11 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { AbstractOneDataSystemAppender, IAppInsightsCore } from '../common/1dsAppender.js';
+// VOID EDITOR DEVELOPER NOTE:
+// This file originally contained Microsoft's proprietary 1DS (One Data Strategy) telemetry service
+// implementation for the browser. This has been replaced with a placeholder implementation.
 
+import { AbstractOneDataSystemAppender, IAppInsightsCore } from '../common/1dsAppender.js';
 
 export class OneDataSystemWebAppender extends AbstractOneDataSystemAppender {
 	constructor(
@@ -14,11 +17,8 @@ export class OneDataSystemWebAppender extends AbstractOneDataSystemAppender {
 		iKeyOrClientFactory: string | (() => IAppInsightsCore), // allow factory function for testing
 	) {
 		super(isInternalTelemetry, eventPrefix, defaultData, iKeyOrClientFactory);
-
-		// If we cannot fetch the endpoint it means it is down and we should not send any telemetry.
-		// This is most likely due to ad blockers
-		fetch(this.endPointHealthUrl, { method: 'GET' }).catch(err => {
-			this._aiCoreOrKey = undefined;
-		});
+		
+		// Void: No telemetry collection in browser
+		console.log('[VOID TELEMETRY] Browser telemetry disabled');
 	}
 }
